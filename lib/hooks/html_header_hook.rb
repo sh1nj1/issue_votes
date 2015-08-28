15fc47754e39910faf0a28fe47f89510a20e4346
Juhani Seppälä <jseppal@student.uef.fi>
2015-08-28 15:44:28 +0300

    Removed global js header hook

--- lib/hooks/html_header_hook.rb
+++ lib/hooks/html_header_hook.rb
@@ -2,8 +2,7 @@ module KohaSuomiIssueVotes
   module Hooks
     class HtmlHeaderHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context={})
-        return javascript_include_tag(:issue_votes, :plugin => 'issue_votes') +
-          stylesheet_link_tag(:issue_votes, :plugin => 'issue_votes')
+        return stylesheet_link_tag(:issue_votes, :plugin => 'issue_votes')
       end
     end
   end
